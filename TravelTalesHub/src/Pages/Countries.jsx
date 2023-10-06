80dcb6c3c14f81fe2119b9d9a773685378f50332
SANJANA <sanjana.js02@gmail.com>
2023-10-07 00:43:33 +0530

    working

--- TravelTalesHub/src/Pages/Countries.jsx
+++ TravelTalesHub/src/Pages/Countries.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  const fetchAllCountries = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/countries`);
+      if (response.ok) {
+        const allCountries = await response.json();
+        setCountries(allCountries);
+        console.log(allCountries)
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
   return (
     <div>
-      
+      <h1>List of Countries</h1>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.id}>{country.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Countries
+export default Countries;
